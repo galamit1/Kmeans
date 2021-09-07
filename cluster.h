17ab9a5a35f8c0b6454a8b1ce120665d8090014c
Gal Amit <galamit2@gmail.com>
2021-09-07 21:12:58 +0300

    add spk

---
+++ cluster.h
@@ -0,0 +1,8 @@
+//
+// Created by galam on 07/09/2021.
+//
+
+#ifndef SPK_CLUSTER_H
+#define SPK_CLUSTER_H
+
+#endif //SPK_CLUSTER_H
---
+++ kmeans.h
@@ -0,0 +1,8 @@
+//
+// Created by galam on 07/09/2021.
+//
+
+#ifndef SPK_KMEANS_H
+#define SPK_KMEANS_H
+
+#endif //SPK_KMEANS_H
